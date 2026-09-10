7ec530df8f28b73fa1a30dbcdd34dc64f4f4160a
IslamNizami <islamnizami0046@gmail.com>
2026-09-10 20:41:03 +0200

    Refactor: Move JWT secret to properties and update gitignore

--- src/main/java/hu/bme/mit/smartmobility/accountshielddevlab/Service/JwtService.java
+++ src/main/java/hu/bme/mit/smartmobility/accountshielddevlab/Service/JwtService.java
@@ -1,43 +1,45 @@
 package hu.bme.mit.smartmobility.accountshielddevlab.Service;
 
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    private final String SECRET_KEY  = "O11LN7IDguGcM6uIwta5bvEpj2yydqENVUVqsYKPAHR";
+    @Value("${jwt.secret}")
+    private String secretKey;
 
-    private SecretKey getSignKey(){
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private SecretKey getSignKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String generateToken(String email){
+    public String generateToken(String email) {
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15)) //15 minutes
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15)) // 15 dakika
                 .signWith(getSignKey())
                 .compact();
     }
 
-    public String extractEmail(String token){
+    public String extractEmail(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = exctractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    private Claims exctractAllClaims(String token){
+    private Claims exctractAllClaims(String token) {
         return Jwts.parser()
                 .verifyWith(getSignKey())
                 .build()
@@ -45,12 +47,12 @@ public class JwtService {
                 .getPayload();
     }
 
-    public boolean isTokenValid(String token, String email){
+    public boolean isTokenValid(String token, String email) {
         final String extractedEmail = extractEmail(token);
         return (extractedEmail.equals(email) && !isTokenExpired(token));
     }
 
-    private boolean isTokenExpired(String token){
+    private boolean isTokenExpired(String token) {
         return exctractAllClaims(token).getExpiration().before(new Date());
     }
 }
